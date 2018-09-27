7ad1e929981b31ee38b273b723d744cd40d02043
unknown <LIU.Nian@sindyware.com>
2018-09-27 18:10:39 +0800

    ...

--- script/index.js
+++ script/index.js
@@ -1,20 +1,25 @@
-var Config = {};
+var Loads = {
+    has: [],
+    loadElement(relys, callback) {
+        var loadIndex = 0;
+        if (relys) {
+            relys.map((item) => {
+                if (this.has.indexOf(item) == -1) {
+                    this.has.push(item);
+                    Common.importScript(item, checkStatus);
+                } else {
+                    checkStatus();
+                }
+            });
+        }
 
-Config.fileRely = {
-    'app': [
-        'script/component/MenuNav.js',
-    ],
-    'home': [
-        'script/page/home.js',
-        'script/component/Loading.js',
-        'script/component/test.js'
-    ],
-    'article': [
-        'script/page/article.js'
-    ],
-    'about': [
-        'script/page/about.js'
-    ]
+        function checkStatus() {
+            loadIndex++;
+            if (loadIndex == relys.length) {
+                callback && callback();
+            }
+        }
+    }
 };
 
 new Vue({
@@ -23,10 +28,10 @@ new Vue({
     template: `
         <div>
             <div class="header">
-                <l-menu-nav v-if="loadComponent" :default-value="pathName" :data="menuData" @menu-click="menuClick"></l-menu-nav>
+                <l-menu-nav v-if="loadRely" :default-value="pathInfo.value" :data="menuData" @menu-click="menuClick"></l-menu-nav>
             </div>
             <div class="main">
-                <page-view :path="c_pathName" />
+                <page-view v-if="pathInfo!=null" :path="'l-page-'+pathInfo.value" />
             </div>
         </div>
     `,
@@ -40,22 +45,30 @@ new Vue({
     },
     data() {
         return {
-            loadComponent: false,
-            pathName: '',
+            loadRely: false,
+            pathInfo: null,
             menuData: [{
                 text: '首页',
-                value: 'home'
+                value: 'home',
+                relys: [
+                    'script/page/home.js'
+                ]
             }, {
                 text: '我的文章',
-                value: 'article'
+                value: 'article',
+                relys: [
+                    'script/page/article.js'
+                ]
             }, {
                 text: '关于',
-                value: 'about'
+                value: 'about',
+                relys: [
+                    'script/page/about.js'
+                ]
             }],
-            loadFile: {
-                has: [],
-                rely: Config.fileRely
-            }
+            relys: [
+                'script/component/MenuNav.js'
+            ]
         }
     },
     watch: {
@@ -63,23 +76,24 @@ new Vue({
             this.loadPage(to.path.substr(1));
         },
     },
-    computed: {
-        c_pathName() {
-            if (this.pathName != '') return 'l-page-' + this.pathName;
-            else return '';
-        }
-    },
     created() {
-        this.loadElement('app', () => {
+        Loads.loadElement(this.relys, () => {
             this.loadPage(this.$route.path.substr(1));
-            this.loadComponent = true;
+            this.loadRely = true;
         });
     },
     methods: {
         loadPage(name) {
             var _name = name == '' ? this.menuData[0].value : name;
-            this.loadElement(_name, () => {
-                this.pathName = _name;
+            var tempItem = this.menuData[0];
+            for (var i = 0; i < this.menuData.length; i++) {
+                if (_name == this.menuData[i].value) {
+                    tempItem = this.menuData[i];
+                    break;
+                }
+            }
+            Loads.loadElement(tempItem.relys, () => {
+                this.pathInfo = tempItem;
             });
         },
         pageJump(value) {
@@ -89,27 +103,6 @@ new Vue({
         },
         menuClick(item) {
             this.pageJump(item.value);
-        },
-        loadElement(name, callback) {
-            var rely = this.loadFile.rely[name];
-            var loadIndex = 0;
-            if (rely) {
-                rely.map((item) => {
-                    if (this.loadFile.has.indexOf(item) == -1) {
-                        this.loadFile.has.push(item);
-                        Common.importScript(item, checkStatus);
-                    } else {
-                        checkStatus();
-                    }
-                });
-            }
-
-            function checkStatus() {
-                loadIndex++;
-                if (loadIndex == rely.length) {
-                    callback && callback();
-                }
-            }
         }
     }
 });
--- script/page/home.js
+++ script/page/home.js
@@ -1,9 +1,23 @@
 Vue.component('l-page-home', {
     template: `
         <div>
-            这个是home页面
-            <l-loading />
-            <l-test />
+            这个是home页面<br />
+            加载组件:loading <l-loading v-if="loadRely" /><br />
+            测试另外一个组件的加载：<l-test v-if="loadRely" />
         </div>
-    `
+    `,
+    data() {
+        return {
+            loadRely: false,
+            relys: [
+                'script/component/Loading.js',
+                'script/component/test.js'
+            ],
+        };
+    },
+    created() {
+        Loads.loadElement(this.relys, () => {
+            this.loadRely = true;
+        });
+    }
 });
