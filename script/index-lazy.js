bc421eb7309aaea2bbc9db3ac9be36216a57fc98
NianLiu <ln8903@outlook.com>
2018-10-16 18:31:07 +0800

    .

---
+++ script/index-lazy.js
@@ -0,0 +1,157 @@
+var Reouter = new VueRouter({
+    routes: [{
+        path: '/',
+        name: ''
+    }, {
+        path: '/home',
+        name: 'home'
+    }, {
+        path: '/article',
+        name: 'article'
+    }, {
+        path: '/article/:id',
+        name: 'article-detail'
+    }, {
+        path: '/about',
+        name: 'about'
+    }]
+});
+
+var ImportFile = {
+    pages: {
+        'home': ['script/page/home.js'],
+        'article': ['script/page/article.js'],
+        'article-detail': ['script/page/article-detail.js'],
+        'about': ['script/page/about.js'],
+    },
+    has: [],
+    page(name, callback) {
+        this.load(this.pages[name], callback);
+    },
+    load(imports, callback) {
+        var loadIndex = 0;
+        if (imports) {
+            imports.map((item) => {
+                if (this.has.indexOf(item) == -1) {
+                    this.has.push(item);
+                    importScript(item, checkStatus);
+                } else {
+                    checkStatus();
+                }
+            });
+        }
+
+        function checkStatus() {
+            loadIndex++;
+            if (loadIndex == imports.length) {
+                callback && callback();
+            }
+        }
+
+        function importScript(url, callback) {
+            var head = document.getElementsByTagName('head')[0];
+            var script = document.createElement('script');
+            // script.type = 'text/javascript';
+            script.src = url;
+            if (typeof (callback) == 'function') {
+                script.onload = script.onreadystatechange = function () {
+                    if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") {
+                        callback();
+                        script.onload = script.onreadystatechange = null;
+                    }
+                };
+            }
+            head.appendChild(script);
+        }
+    }
+};
+
+var MixinImport = {
+    created() {
+        ImportFile.load(this.importObject.data, () => {
+            this.importObject.status = true;
+        });
+    }
+};
+
+new Vue({
+    el: '#app',
+    router: Reouter,
+    mixins: [MixinImport],
+    template: `
+        <div v-if="importObject.status">
+            <div class="header">
+                <l-menu-nav 
+                    v-if="currentMenu" 
+                    :default-value="currentMenu" 
+                    :data="menuData" 
+                    @menu-click="menuClick"
+                ></l-menu-nav>
+            </div>
+            <div class="main">
+                <page-view 
+                    v-if="currentPage" 
+                    :path="currentPage" 
+                />
+            </div>
+        </div>
+    `,
+    components: {
+        'page-view': {
+            props: ['path'],
+            render: function (h) {
+                return h(this.path);
+            }
+        }
+    },
+    data() {
+        return {
+            importObject: {
+                status: false,
+                data: [
+                    'script/tween.js',
+                    'script/common.js',
+                    'script/component/menu-nav.js'
+                ]
+            },
+            currentMenu: null,
+            currentPage: null,
+            menuData: [{
+                text: '首页',
+                value: 'home'
+            }, {
+                text: '我的文章',
+                value: 'article'
+            }, {
+                text: '关于',
+                value: 'about'
+            }]
+        }
+    },
+    watch: {
+        $route: function (to) {
+            this.loadPage(to.name);
+        },
+    },
+    created() {
+        this.loadPage(this.$route.name);
+    },
+    methods: {
+        loadPage(name) {
+            var _name = name == '' ? this.menuData[0].value : name;
+            ImportFile.page(_name, () => {
+                this.currentMenu = _name.split('-')[0];
+                this.currentPage = 'l-page-' + _name;
+            });
+        },
+        pageJump(name, params) {
+            this.$router.push({
+                name: name,
+                params: params
+            });
+        },
+        menuClick(item) {
+            this.pageJump(item.value);
+        }
+    }
+});
--- script/index.js
+++ script/index.js
@@ -1,33 +1,41 @@
-var Reouter = new VueRouter({
-    routes: [{
-        path: '/',
-        name: ''
-    }, {
-        path: '/home',
-        name: 'home'
-    }, {
-        path: '/article',
-        name: 'article'
-    }, {
-        path: '/article/:id',
-        name: 'article-detail'
-    }, {
-        path: '/about',
-        name: 'about'
-    }]
-});
-
 var ImportFile = {
-    pages: {
-        'home': ['script/page/home.js'],
-        'article': ['script/page/article.js'],
-        'article-detail': ['script/page/article-detail.js'],
-        'about': ['script/page/about.js'],
+    pageRouter: null,
+    init(next) {
+        var pages = [
+            'script/page/home.js',
+            'script/page/article.js',
+            'script/page/article-detail.js',
+            'script/page/about.js'
+        ];
+
+        this.load(pages, () => {
+            this.pageRouter = new VueRouter({
+                routes: [{
+                    path: '/',
+                    name: '',
+                    component: PageHome
+                }, {
+                    path: '/home',
+                    name: 'home',
+                    component: PageHome
+                }, {
+                    path: '/article',
+                    name: 'article',
+                    component: PageArticle
+                }, {
+                    path: '/article/:id',
+                    name: 'article-detail',
+                    component: PageArticleDetail
+                }, {
+                    path: '/about',
+                    name: 'about',
+                    component: PageAbout
+                }]
+            })
+            next && next();
+        });
     },
     has: [],
-    page(name, callback) {
-        this.load(this.pages[name], callback);
-    },
     load(imports, callback) {
         var loadIndex = 0;
         if (imports) {
@@ -51,7 +59,6 @@ var ImportFile = {
         function importScript(url, callback) {
             var head = document.getElementsByTagName('head')[0];
             var script = document.createElement('script');
-            // script.type = 'text/javascript';
             script.src = url;
             if (typeof (callback) == 'function') {
                 script.onload = script.onreadystatechange = function () {
@@ -74,84 +81,70 @@ var MixinImport = {
     }
 };
 
-new Vue({
-    el: '#app',
-    router: Reouter,
-    mixins: [MixinImport],
-    template: `
-        <div v-if="importObject.status">
-            <div class="header">
-                <l-menu-nav 
-                    v-if="currentMenu" 
-                    :default-value="currentMenu" 
-                    :data="menuData" 
-                    @menu-click="menuClick"
-                ></l-menu-nav>
-            </div>
-            <div class="main">
-                <page-view 
-                    v-if="currentPage" 
-                    :path="currentPage" 
-                />
-            </div>
-        </div>
-    `,
-    components: {
-        'page-view': {
-            props: ['path'],
-            render: function (h) {
-                return h(this.path);
+ImportFile.init(() => {
+    new Vue({
+        el: '#app',
+        router: ImportFile.pageRouter,
+        mixins: [MixinImport],
+        template: `
+                <div v-if="importObject.status">
+                    <div class="header">
+                        <l-menu-nav 
+                            v-if="currentMenu" 
+                            :default-value="currentMenu" 
+                            :data="menuData" 
+                            @menu-click="menuClick"
+                        ></l-menu-nav>
+                    </div>
+                    <div class="main">
+                        <router-view></router-view>
+                    </div>
+                </div>
+            `,
+        data() {
+            return {
+                importObject: {
+                    status: false,
+                    data: [
+                        'script/tween.js',
+                        'script/common.js',
+                        'script/component/menu-nav.js'
+                    ]
+                },
+                currentMenu: null,
+                menuData: [{
+                    text: '首页',
+                    value: 'home'
+                }, {
+                    text: '我的文章',
+                    value: 'article'
+                }, {
+                    text: '关于',
+                    value: 'about'
+                }]
+            }
+        },
+        watch: {
+            currentMenu(n) {
+                if (n == '') this.currentMenu = 'home';
+            },
+            $route(to) {
+                this.currentMenu = to.name;
+            },
+        },
+        created() {
+            this.currentMenu = this.$route.name;
+        },
+        methods: {
+            pageJump(name, params) {
+                this.$router.push({
+                    name: name,
+                    params: params
+                });
+            },
+            menuClick(item) {
+                this.pageJump(item.value);
             }
         }
-    },
-    data() {
-        return {
-            importObject: {
-                status: false,
-                data: [
-                    'script/tween.js',
-                    'script/common.js',
-                    'script/component/menu-nav.js'
-                ]
-            },
-            currentMenu: null,
-            currentPage: null,
-            menuData: [{
-                text: '首页',
-                value: 'home'
-            }, {
-                text: '我的文章',
-                value: 'article'
-            }, {
-                text: '关于',
-                value: 'about'
-            }]
-        }
-    },
-    watch: {
-        $route: function (to) {
-            this.loadPage(to.name);
-        },
-    },
-    created() {
-        this.loadPage(this.$route.name);
-    },
-    methods: {
-        loadPage(name) {
-            var _name = name == '' ? this.menuData[0].value : name;
-            ImportFile.page(_name, () => {
-                this.currentMenu = _name.split('-')[0];
-                this.currentPage = 'l-page-' + _name;
-            });
-        },
-        pageJump(name, params) {
-            this.$router.push({
-                name: name,
-                params: params
-            });
-        },
-        menuClick(item) {
-            this.pageJump(item.value);
-        }
-    }
+    });
 });
